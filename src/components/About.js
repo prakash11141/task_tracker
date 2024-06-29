4a2ee828cc341512b593e04505a825bdd0caf02c
Prakash <111prakashshrestha11141@gmail.com>
2024-06-29 22:30:03 +0545

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div>
-      <h4>Version 1.0.0</h4>
-      <Link to='/'>Go Back</Link>
+      <Typography>
+        This is a task tracker Application to keep record of tasks.
+      </Typography>
+      <h4>Version 4.0.0</h4>
+      <Link to="/">Go Back</Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
